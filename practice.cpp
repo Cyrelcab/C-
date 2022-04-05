4385e19f6d406289d06937fa62ad471d01b941a4
Cyrel Cabodbod <101546665+Cyrelcab@users.noreply.github.com>
2022-04-05 17:55:22 +0800

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -46,3 +46,94 @@ int main() {
     
     return 0;
 }
+
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <cmath>
+#define pi 3.14
+/*Write a program that can compute the Perimeter, Area,
+Circumference of a shape that have been choosen*/
+
+using namespace std;
+
+int main()
+{
+    int shape, length, width, base, side, height, radius,
+    Perimeter1, Area1, Perimeter2, Area2, Perimeter3, Area3, Circumference, Area4;
+    
+    cout<<"Select a number of what shape need to be compute: \n";
+    cout << "1. Square\n";
+    cout << "2. Rectangle\n";
+    cout << "3. Isosceles Triangle\n";
+    cout << "4. Circle\n";
+    cout << "Enter your choice: ";
+    cin >> shape;
+    
+    if(shape > 4){
+        cout << "The number you've entered is invalid!";
+    }else{
+        switch(shape){
+            case 1:
+                cout << "\nYou've choose Square ";
+                cout << "\nPlease input the Length of Side: ";
+                cin >> length;
+                
+                Perimeter1 = 4 * length;
+                Area1 = pow(length,2);
+                
+                cout << "\nPerimeter =  " << Perimeter1 << endl;
+                cout << "Area = " << Area1 << endl;
+            break;
+            case 2:
+                cout << "\nYou've choose Rectangle";
+                cout << "\nPlease input the length of the rectangle: ";
+                cin >> length;
+                cout << "\nPlease input the width of the rectangle: ";
+                cin >> width;
+                
+                Perimeter2 = (2*width)+(2*length);
+                Area2 = width*length;
+                
+                cout << "\nPerimeter = " << Perimeter2 << endl;
+                cout << "Area = " << Area2 << endl;
+            break;
+            case 3:
+                cout << "\nYou've choose Isosceles Triangle";
+                cout << "\nPlease input the base of the Isosceles Triangle: ";
+                cin >> base;
+                cout << "\nPlease input the side of the Isosceles Triangle: ";
+                cin >> side;
+                cout << "\nPlease input the height of the Isosceles Triangle: ";
+                cin >> height;
+                
+                Perimeter3 = (2*side)+base;
+                Area3 = base*height*0.5;
+                
+                cout << "\nPerimeter = " << Perimeter3 << endl;
+                cout << "Area = " << Area3 << endl;
+            break;
+            case 4:
+                cout << "\nYou've choose Circle";
+                cout << "\nPlease input the radius of the circle: ";
+                cin >> radius;
+                
+                Circumference = 2*pi*radius;
+                Area4 = pi*pow(radius,2);
+                
+                cout << "\nThe circumference of the circle is " << Circumference << endl;
+                cout << "The area of the circle is " << Area4 << endl;
+            break;
+                
+            
+        }
+    }
+
+    return 0;
+}
